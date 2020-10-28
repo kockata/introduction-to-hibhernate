7b82f7afc8ec9c5d753a9ec6cffbbf18395807da
kocem <kocem@DESKTOP-VDMPBQO>
2020-10-28 22:02:49 +0200

    withNewTask k

---
+++ src/main/java/entitiesNew/sales/StoreLocation.java
@@ -0,0 +1,39 @@
+//package entitiesNew.sales;
+//
+//import entitiesNew.BaseEntity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.OneToMany;
+//import javax.persistence.Table;
+//import java.util.Set;
+//
+//@Entity
+//@Table(name = "store_locations")
+//public class StoreLocation extends BaseEntity {
+//    private String location_name;
+//    private Set<Sale> sales;
+//
+//    public StoreLocation(){
+//
+//    }
+//
+//    @Column(name = "location_name")
+//    public String getLocation_name() {
+//        return location_name;
+//    }
+//
+//    public void setLocation_name(String location_name) {
+//        this.location_name = location_name;
+//    }
+//
+//
+//    @OneToMany(targetEntity = Sale.class, mappedBy = "storeLocation")
+//    public Set<Sale> getSales() {
+//        return sales;
+//    }
+//
+//    public void setSales(Set<Sale> sales) {
+//        this.sales = sales;
+//    }
+//}
